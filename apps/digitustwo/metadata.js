e0399fd4702ee91412fad12d216ad940e97e2607
Andres Cabral <cabral.andres@gmail.com>
2022-05-24 13:32:05 -0400

    Adding other things

---
+++ apps/digitustwo/metadata.js
@@ -0,0 +1,12 @@
+{ "id": "digitustwo",
+  "name": "Digitus Two clock",
+  "shortName":"DigitusTwo",
+  "icon": "app.png",
+  "version":"0.01",
+  "description": "This is a remake of my original pebble watchface",
+  "tags": "",
+  "storage": [
+    {"name":"digitustwo.app.js","url":"app.js"},
+    {"name":"digitustwo.img","url":"app-icon.js","evaluate":true}
+  ]
+}
